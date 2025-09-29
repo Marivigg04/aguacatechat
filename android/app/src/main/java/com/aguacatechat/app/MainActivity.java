ae519c195dbc7eee010c53763d13b8cfe3bdd827
YoshiHD4K <tomyreveroncito@gmail.com>
2025-09-29 18:09:17 -0400

    Creacion del proyecto Android

---
+++ android/app/src/main/java/com/aguacatechat/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aguacatechat.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.cjs
@@ -0,0 +1,10 @@
+/** Configuración Capacitor usando CommonJS para proyecto con type=module */
+module.exports = {
+  appId: 'com.aguacatechat.app',
+  appName: 'AguacateChat',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https'
+  }
+};
